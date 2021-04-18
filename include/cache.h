3f42bb2ab5f96baceb81deacbd097d36797a0724
Anna Syuris <ann.syuris@gmail.com>
2021-04-19 00:29:14 +0300

    clang format

--- include/cache.h
+++ include/cache.h
@@ -1,33 +1,40 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
+#include <list>
 #include <new>
 #include <ostream>
-#include <list>
-#include <algorithm>
 
-template<class Key, class KeyProvider, class Allocator>
-class Cache {
+template <class Key, class KeyProvider, class Allocator>
+class Cache
+{
 public:
-    template<class... AllocArgs>
+    template <class... AllocArgs>
     Cache(const std::size_t cache_size, AllocArgs &&... alloc_args)
-            : m_max_top_size(cache_size), m_max_low_size(cache_size), m_alloc(std::forward<AllocArgs>(alloc_args)...) {
+        : m_max_top_size(cache_size)
+        , m_max_low_size(cache_size)
+        , m_alloc(std::forward<AllocArgs>(alloc_args)...)
+    {
     }
 
-    std::size_t size() const {
-        return m_top_queue.size() + m_low_queue.size(); //????????????
+    std::size_t size() const
+    {
+        return m_top_queue.size() + m_low_queue.size();
     }
 
-    bool empty() const {
-        return m_top_queue.empty() && m_low_queue.empty(); //????????????
+    bool empty() const
+    {
+        return m_top_queue.empty() && m_low_queue.empty();
     }
 
-    template<class T>
-    T &get(const Key &key);
+    template <class T>
+    T & get(const Key & key);
 
-    std::ostream &print(std::ostream &strm) const;
+    std::ostream & print(std::ostream & strm) const;
 
-    friend std::ostream &operator<<(std::ostream &strm, const Cache &cache) {
+    friend std::ostream & operator<<(std::ostream & strm, const Cache & cache)
+    {
         return cache.print(strm);
     }
 
@@ -39,21 +46,22 @@ private:
     std::list<KeyProvider *> m_low_queue;
 };
 
-// template<typename T, typename = std::enable_if<std::is_base_of<KeyProvider, T>::value>>
-template<class Key, class KeyProvider, class Allocator>
-template<class T>
-inline T &Cache<Key, KeyProvider, Allocator>::get(const Key &key) {
-    auto it = std::find_if(m_top_queue.begin(), m_top_queue.end(), [&key](const KeyProvider *elem) {
+template <class Key, class KeyProvider, class Allocator>
+template <class T>
+inline T & Cache<Key, KeyProvider, Allocator>::get(const Key & key)
+{
+    auto it = std::find_if(m_top_queue.begin(), m_top_queue.end(), [&key](const KeyProvider * elem) {
         return *elem == key;
     });
 
-    if (it != m_top_queue.end()) { // то бишь нашли элемент в прив очереди
-        while (it != m_top_queue.begin()) { // пихаем нужный элемент в начало очереди
+    if (it != m_top_queue.end()) {
+        while (it != m_top_queue.begin()) {
             const auto old = it--;
             std::iter_swap(it, old);
         }
-    } else {
-        it = std::find_if(m_low_queue.begin(), m_low_queue.end(), [&key](const KeyProvider *elem) {
+    }
+    else {
+        it = std::find_if(m_low_queue.begin(), m_low_queue.end(), [&key](const KeyProvider * elem) {
             return *elem == key;
         });
 
@@ -65,23 +73,25 @@ inline T &Cache<Key, KeyProvider, Allocator>::get(const Key &key) {
             m_top_queue.push_front(*it);
 
             m_low_queue.erase(it);
-        } else {
+        }
+        else {
             if (m_low_queue.size() == m_max_low_size) {
                 m_alloc.template destroy<T>(m_low_queue.back());
                 m_low_queue.pop_back();
             }
 
             m_low_queue.push_front(m_alloc.template create<T>(key));
-            return *dynamic_cast<T*>(m_low_queue.front());
+            return *dynamic_cast<T *>(m_low_queue.front());
         }
     }
 
-    return *dynamic_cast<T*>(m_top_queue.front());
-//    throw std::bad_alloc{};
+    return *dynamic_cast<T *>(m_top_queue.front());
+    //    throw std::bad_alloc{};
 }
 
-template<class Key, class KeyProvider, class Allocator>
-inline std::ostream &Cache<Key, KeyProvider, Allocator>::print(std::ostream &strm) const {
+template <class Key, class KeyProvider, class Allocator>
+inline std::ostream & Cache<Key, KeyProvider, Allocator>::print(std::ostream & strm) const
+{
     return strm << "Priority: <empty>"
                 << "\nRegular: <empty>"
                 << "\n";
--- include/pool.h
+++ include/pool.h
@@ -2,47 +2,50 @@
 
 #include <cstddef>
 #include <initializer_list>
+#include <map>
 #include <new>
 #include <vector>
-#include <map>
 
 namespace pool {
 
-    class Pool;
+class Pool;
 
-    Pool *create_pool(std::size_t obj_size, std::size_t obj_count);
+Pool * create_pool(std::size_t obj_size, std::size_t obj_count);
 
-    void destroy_pool(Pool *pool);
+void destroy_pool(Pool * pool);
 
-    std::size_t pool_obj_size(const Pool &pool);
+std::size_t pool_obj_size(const Pool & pool);
 
-    void *allocate(Pool &pool, std::size_t n);
+void * allocate(Pool & pool, std::size_t n);
 
-    bool contains(Pool &pool, const void *ptr);
+bool contains(Pool & pool, const void * ptr);
 
-    void deallocate(Pool &pool, const void *ptr, std::size_t n);
+void deallocate(Pool & pool, const void * ptr, std::size_t n);
 
-} // pool namespace
+} // namespace pool
 
-class PoolAllocator {
+class PoolAllocator
+{
 public:
-
     PoolAllocator(const std::size_t count, std::initializer_list<std::size_t> sizes)
-            : m_count(count) {
+        : m_count(count)
+    {
         for (size_t cur : sizes) {
             pools[cur] = pool::create_pool(cur, count / cur);
         }
     }
 
-    void *allocate(std::size_t n) {
+    void * allocate(std::size_t n)
+    {
         if (pools.find(n) != pools.end()) {
             return pool::allocate(*pools[n], 1);
         }
-//        return nullptr;
+        //        return nullptr;
         throw std::bad_alloc{};
     }
 
-    void deallocate(const void *ptr) {
+    void deallocate(const void * ptr)
+    {
         for (auto pool : pools) {
             if (pool::contains(*pool.second, ptr)) {
                 pool::deallocate(*pool.second, ptr, pool.first);
@@ -51,7 +54,7 @@ public:
     }
 
 private:
-//    std::initializer_list<std::size_t> m_sizes;
+    //    std::initializer_list<std::size_t> m_sizes;
     std::size_t m_count;
-    std::map<size_t , pool::Pool *> pools;
+    std::map<size_t, pool::Pool *> pools;
 };
